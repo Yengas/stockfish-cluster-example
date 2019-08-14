c54b016d0d569b874ae923906ee6bac0c62d1fc9
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2019-08-14 19:19:35 +0300

    changed to child_process for clustering stragety.

--- benchmark.js
+++ benchmark.js
@@ -1,7 +1,7 @@
 async function benchmark(chessBackend) {
 	const fen = 'N7/P3pk1p/3p2p1/r4p2/8/4b2B/4P1KP/1R6 w - - 0 34';
 	const level = 20;
-	const benchmarkCount = 1000;
+	const benchmarkCount = 10000;
 	const currentTime = Date.now();
 
 	await Promise.all([...new Array(benchmarkCount)].map(() => chessBackend.getBestMove(fen, level)));
---
+++ child_process.js
@@ -0,0 +1,18 @@
+// entrypoint for running workers with the child process strategy. master process will handle client communication
+// and there will be *NUM_OF_WORKERS* amount of worker processes to run stockfish
+const child_process = require('child_process');
+const benchmark = require('./benchmark');
+const server = require('./index');
+const isBenchmark = process.env.BENCHMARK === 'true';
+const os = require('os');
+const NUM_OF_WORKERS = Math.max(os.cpus().length - 1, 1);
+const clusterMaster = require('./child_process/master');
+
+for (let i = 0; i < NUM_OF_WORKERS; i++) {
+	const worker = child_process.fork(require.resolve('./child_process/worker'));
+
+	clusterMaster.addWorker(worker);
+}
+
+const chessBackend = require('./child_process/index');
+(isBenchmark ? benchmark : server)(chessBackend);
