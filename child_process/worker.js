ef6ac5ea1dbfabb49b0e7e661f506cf4151044ec
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2019-08-15 13:37:59 +0300

    simplify child_process worker and single using promise chaining.

--- child_process/worker.js
+++ child_process/worker.js
@@ -5,8 +5,6 @@ async function createReply({ fen, level, depth }) {
 	return getBestMove(fen, level, depth);
 }
 
-let inProgress = false;
-
 function requestWork() {
 	process.send({ type: WorkerActions.RequestWork });
 }
@@ -15,26 +13,25 @@ function sendResponse(workId, reply) {
 	process.send({ type: WorkerActions.ReplyBestMove, workId, reply });
 }
 
-async function doWork({ id: workId, params }) {
-	// can not handle more than one work at once, because there is one chess engine per worker
-	if (inProgress === true) throw new Error('can not process more than one at the same time');
-	inProgress = true;
+let work = Promise.resolve();
 
+async function doWork({ id: workId, params }) {
 	try {
 		const reply = await createReply(params);
 
 		sendResponse(workId, reply);
 	} catch(err) {
 		console.error('there was an error when processing work:', err.message);
-	} finally {
-		inProgress = false;
 	}
 }
 
 process.on('message', async (action) => {
 	if (action.type === MasterActions.SendWork) {
-		await doWork(action.work).catch(console.error);
-		requestWork();
+		work = work.then(() => {
+			return doWork(action.work)
+				.catch(console.error)
+				.then(() => requestWork());
+		});
 	}
 });
 
--- single/index.js
+++ single/index.js
@@ -1,30 +1,8 @@
 const { getBestMove: realGetBestMove } = require('../stockfish')();
-const queue = [];
-let inProgress = false;
-
-async function doWork() {
-	if (inProgress || queue.length === 0) return false;
-	inProgress = true;
-
-	try {
-		const { params: { fen, level, depth }, resolve } = queue.shift();
-		const reply = await realGetBestMove(fen, level, depth);
-
-		resolve(reply);
-	} catch(err) {
-		console.error('error processing messages:', err.message);
-	} finally {
-		inProgress = false;
-	}
-
-	if (queue.length > 0) setTimeout(doWork, 0);
-}
+let work = Promise.resolve();
 
 module.exports = {
 	async getBestMove(fen, level, depth) {
-		return new Promise(resolve => {
-			queue.push({ params: { fen, level, depth }, resolve });
-			doWork();
-		});
+		return work = work.then(() => realGetBestMove(fen, level, depth));
 	},
 };
